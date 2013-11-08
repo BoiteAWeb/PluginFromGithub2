3cb00c4c09711c8c945047a4a1859679dc265ad3
Julio Potier <juliobosk@gmail.com>
2013-11-08 14:50:14 +0100

    willy

--- PluginFromGithub2.php
+++ PluginFromGithub2.php
@@ -7,6 +7,6 @@ Author: Julio Potier
 add_action( 'wp_footer', '___wp_footer', 11 );
 function ___wp_footer(){
 	global $wp_version;
-	printf( '<p>%s</p>', $wp_version );
+	printf( '<p>%s Willy</p>', $wp_version );
 }
 ?>
